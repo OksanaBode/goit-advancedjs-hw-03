3a7ebc8a97d6d387ee5d5606a0374c4274e3fcbb
OksanaBode <ksiushka1@gmail.com>
2025-06-08 17:55:02 +0200

    package.json correction

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import iziToast from 'izitoast';
+import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { fetchImages } from './js/pixabay-api';
 import { createGalleryCard } from './js/render-functions';
